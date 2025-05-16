ad1455eb07ad4847369c0afc8713800bdad4fdb1
lokajan <116150214+lokajan@users.noreply.github.com>
2025-05-16 17:12:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// console.log(451);
+
+/*
+let obj = {
+    fname: "jon",
+    age: 16
+}
+
+
+   let food = ["aa", "bb", "cc"];
+console.log(obj);
+console.log(food[1]);
+*/
+
+/* function add(num1, num2) {
+   num3 = num1 + num2;
+
+   console.log(num3);
+
+}
+add(49, 89)
+
+console.log(true == 5);
+*/
+let old = 20;
+let type = old > 22 ? "20+" : "20-";
+
+console.log(type);
+
+
+
+let money = 620;
+
+switch (true) {
+
+    case (money > 60000):
+        console.log("ipone");
+        break;
+
+    case (money > 10000):
+        console.log("android");
+        break;
+
+
+    case (money > 5000 && money < 10000):
+        console.log("basic phone");
+        break;
+
+    default:
+        console.log("no chocie");
+}
