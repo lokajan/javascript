b183762093d61906642aea621a4580263484b7f8
lokajan <116150214+lokajan@users.noreply.github.com>
2025-06-01 20:16:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+/*
+window.prompt("name please");
+window.alert("nbvj")
+window.print("bnn")
+
+
+console.log(screen.height, screen.width);
+
+
+
+window.setInterval(() => {
+location.href = "https://www.google.co.uk/"
+}, 5000);
+*/
+/*
+
+const person = {
+    name: "aaaa",
+    place: "wwww",
+    greet() {
+        return `my name is ${this.name} and im coming from ${this.place} //...`;
+
+    }
+};
+let nameVar = prompt("what is ur name");
+let placeVar = prompt("where r u from");
+
+person.name = nameVar;
+person.place = placeVar;
+
+document.body.innerHTML = person.greet();
+*/
