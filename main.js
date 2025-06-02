61d300275bd6113d060c034d620417066142ea15
lokajan <116150214+lokajan@users.noreply.github.com>
2025-06-02 21:45:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,73 @@ person.place = placeVar;
 
 document.body.innerHTML = person.greet();
 */
+
+/*
+
+let food1 = document.getElementById("food");
+let saved = document.getElementById("save");
+
+let foodContainer = document.getElementById("foodContainer")
+
+
+
+
+
+
+
+
+saved.addEventListener("click", () => {
+    // alert("ur saved ")
+    foodContainer.innerHTML += `<li>${food1.value}</li>`;
+
+
+})
+*/
+
+
+let mainn = document.getElementById("mainn");
+
+function createElement1(message) {
+
+    let div = document.createElement("div");
+    let text1 = document.createTextNode(message);
+
+    div.className = "alert";
+
+    div.append(text1);
+    mainn.prepend(div);
+
+    mainn.append(div);
+}
+createElement1("fnsdgid")
+
+
+function temp(msg) {
+    let template = `
+<div>
+<strong>
+${msg}
+</strong>
+</div>
+
+`;
+    mainn.innerHTML += template;
+}
+temp("this is from tempory litteral....")
+
+
+/*
+let qs = document.querySelector(".alert1");
+
+console.log(qs.innerHTML);
+
+console.log(qs.innerText);
+console.log(qs.textContent);
+
+
+
+qs.innerHTML = "gfdg";
+qs.innerText = "fsjg";
+qs.textContent = "trytu";
+*/
+
