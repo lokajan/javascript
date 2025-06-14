fd573d5f0470375d175b7ed10635d793bf1b05be
lokajan <116150214+lokajan@users.noreply.github.com>
2025-06-14 10:49:30 +0530

    Add files via upload

---
+++ classcp.jsx
@@ -0,0 +1,17 @@
+
+
+/*
+import React from "react"
+
+class Compenetss extends React.Component {
+    render() {
+        return < h1 > hxdgsdg</h1 >
+
+    }
+}
+export default Compenetss;
+*/
+
+export default function Cpts() {
+    return <h1>functional cpmrs</h1>
+}
---
+++ helloworld.jsx
@@ -0,0 +1,3 @@
+export default function Helloworld() {
+    return <h1>helloworld.....</h1>;
+}
